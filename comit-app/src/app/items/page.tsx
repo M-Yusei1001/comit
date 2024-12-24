4d626f0a00597134ef125254e7391250e11ff8a6
M-Yusei1001 <bitterchocolate1001@gmail.com>
2024-12-24 07:41:30 +0000

    wip: ファイル作成

---
+++ comit-app/src/app/items/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Page() {
+    return (
+        <div className='my-4'>
+            <div className='max-w-lg mx-auto'>
+                <div className='w-4/5 mx-auto'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
