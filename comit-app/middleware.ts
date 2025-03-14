f9ef98cee552b832379fc2995452c75022aa6de9
Ogata <shingoogata0523@gmail.com>
2025-03-14 23:08:15 +0900

    Correct middleware

--- comit-app/middleware.ts
+++ comit-app/middleware.ts
@@ -1,22 +1,3 @@
-import { auth } from "@/auth";
-import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
-
-export default auth(async (req: NextRequest) => {
-  // セッション情報を取得
-  const session = await auth();
-  
-  // 未認証のユーザーはログインページにリダイレクト
-  if (!session) {
-    console.log("ユーザーは未認証です。ログインページにリダイレクトします。");
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-  
-  // 認証済みの場合はそのまま次の処理へ進む
-  return NextResponse.next();
-});
-
-// matcherで特定のパスにのみミドルウェアを適用
 export const config = {
-  matcher: ["/dashboard/:path*"],
-};
+    matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  };
