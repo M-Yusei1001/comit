bc3377421b0a5aa407b887559d6b8f13b11cf3b2
Ogata <shingoogata0523@gmail.com>
2025-03-18 21:41:45 +0900

    temp

--- comit-app/auth.ts
+++ comit-app/auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/prisma"
- 
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
